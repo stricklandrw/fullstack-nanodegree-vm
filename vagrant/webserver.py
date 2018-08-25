cfc03a0cd0a150574152281503bd0ec384437ea7
Russ Strickland <stricklandrw@gmail.com>
2018-08-24 23:11:27 -0400

    CRUD server objective 1

---
+++ vagrant/webserver.py
@@ -0,0 +1,80 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                restaurants = session.query(Restaurant.name)
+                print restaurants
+                for restaurant in restaurants:
+                    output += "<h2>%s</h2>" % restaurant
+#                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+        except IOError:
+            self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'></form>"
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+
+        except:
+            pass
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('',port), webserverHandler)
+        print "Web server running on port %s" % port
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print "^C entered, stopping web server..."
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
