40f6ba110605cb6072dd2ebe0db7b528e995ec1c
Russ Strickland <stricklandrw@gmail.com>
2018-08-29 21:06:02 -0400

    project.py flask route structure buildout

--- vagrant/project.py
+++ vagrant/project.py
@@ -36,14 +36,17 @@ def restaurantMenu(restaurant_id):
         output += '</br>'
     return output
 
-    def newMenuItem(restaurant_id):
-        return "page to create a new menu item. Task 1 complete!"
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
 
-    def editMenuItem(restaurant_id):
-        return "page to edit a new menu item. Task 2 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a new menu item. Task 2 complete!"
 
-    def deleteMenuItem(restaurant_id):
-        return "page to delete a new menu item. Task 3 complete!"
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a new menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
