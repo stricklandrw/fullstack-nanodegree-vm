f0ffa11150ac8a6bdc552be7b2cb99d8f9bc380d
Russ Strickland <stricklandrw@gmail.com>
2018-08-31 22:46:40 -0400

    JSON menu item api call

--- vagrant/project.py
+++ vagrant/project.py
@@ -69,6 +69,12 @@ def restaurantMenuJSON(restaurant_id):
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id).all()
     return jsonify(MenuItems=[i.serialize for i in items])
 
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def restaurantMenuItemJSON(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(id = menu_id).one()
+    return jsonify(MenuItem=items.serialize)
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
